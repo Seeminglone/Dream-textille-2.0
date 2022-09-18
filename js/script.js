b07c691ad14bb3fce2a3ca469d2b15e2e3932388
Seeminglone <yurakravtsov833@gmail.com>
2022-09-18 18:20:05 +0200

    samle code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const modalBtn = document.querySelector(".log-in-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
+const inputLogin = document.querySelector(".login");
+const inputPassword = document.querySelector(".password")
 
 navToggle.addEventListener('click', function(){
     if(links.classList.contains('show-links')){
@@ -7,5 +12,38 @@ navToggle.addEventListener('click', function(){
     } else {
         links.classList.add("show-links");
     }
-    // links.classList.toggle("show-links");
 });
+
+modalBtn.addEventListener('click', function(){
+modalOverlay.classList.toggle("open-modal");
+});
+
+closeBtn.addEventListener("click", function () {
+    modalOverlay.classList.remove("open-modal");
+});
+
+function myFunction() {
+  let x = document.getElementById("myInputPassword");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+const btns = document.querySelectorAll(".show-password-btn");
+btns.forEach(function(btn){
+  btn.addEventListener("click", function(e){
+    const test = e.currentTarget;
+    test.classList.toggle("show-password");
+  })
+})
+
+const autorizationBtn = document.querySelector(".autorization-btn");
+autorizationBtn.addEventListener("click", function(){
+  let arrLogin = [];
+  let arrPassword = [];
+  arrLogin.push(inputLogin.value)
+  arrPassword.push(inputPassword.value);
+  console.log("Login:" + arrLogin.join('') + " " + "Password:" + arrPassword.join(''));
+})
